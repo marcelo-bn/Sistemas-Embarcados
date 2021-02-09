8228180bbbffc724143138d7c397959b4692cadf
Marcelo Bittencourt <35068186+marcelo-bn@users.noreply.github.com>
2021-02-09 12:45:32 -0300

    Add files via upload

--- Atividades/temporizacao/timer-01/timer-01.ino
+++ Atividades/temporizacao/timer-01/timer-01.ino
@@ -2,20 +2,21 @@
  * Solução para utilizar millis()
  * tratando o problema de overflow
  */
+int led_pin = 7;
 int ledState = LOW;
 unsigned long prevMillis = 0;
+unsigned long maxMillis = 4294967295;
 const long interval = 1000;
 
 void setup() {
   Serial.begin(9600);
-  pinMode(13, OUTPUT);
+  pinMode(led_pin, OUTPUT);
 }
 
 void loop() {
   unsigned long currMillis = millis();
   if (prevMillis > currMillis) { // Verifica se ocorreu overflow
-    Serial.println("overflow");
-    prevMillis = 0;
+    prevMillis = maxMillis - prevMillis; // Diferença entre prevMillis e overflow
     blink(currMillis);
   } else {
     blink(currMillis);
@@ -26,6 +27,6 @@ void blink(unsigned long currMillis) {
   if (currMillis - prevMillis >= interval) {
     prevMillis = currMillis;
     ledState = !ledState;
-    digitalWrite(13, ledState);
+    digitalWrite(led_pin, ledState);
   }
 }
